112fcdbba2e9fbf7b206c41ba14b4cccdcdc27fb
Alcea <maks.komendant@yandex.ru>
2023-03-10 22:39:55 +0300

    dynamic(bad)

--- src/Logic.java
+++ src/Logic.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Logic {
     private static List<Integer> states = Arrays.asList(0);
     public static void cycle(ArrayList<String> text) {
-        text = newLife(text);
+        //text = clear(text);
         for(int row = 0; row < text.size(); row++){
             String buf = "";
             for(int col = 0; col < text.get(row).length(); col++) {
@@ -81,99 +81,113 @@ public class Logic {
                     }
                 }
             }
+            text = newLife(text);
             text.set(row, buf);
-            text = clear(text);
+            //text = clear(text);
         }
         Main.setArray(text);
+        Main.start();
+    }
+    private static ArrayList<String> newLife(ArrayList<String> text){
+        //border
+        String str = new String(new char[text.get(0).length() + 2]).replace("\0", "0");
+        text.add(0,str);
+        text.add(str);
+        for(int row = 1; row < text.size() - 1; row++){
+            text.set(row, "0" + text.get(row) + "0");
+        }
         for(int i = 0; i < text.size(); i++){
             for(int j = 0; j < text.get(i).length(); j++){
                 System.out.print(text.get(i).charAt(j) + " ");
             }
             System.out.println("");
         }
-        Main.start();
-    }
-    private static ArrayList<String> newLife(ArrayList<String> text){
-        //vertical
-        try {
-            for (int row = 1; row < text.size() - 1; row++) {
-                if (text.get(row).charAt(0) == '1' && text.get(row - 1).charAt(0) == '1'
-                        && text.get(row + 1).charAt(0) == '1') {
-                    text.set(row, "1" + text.get(row));
-                    for (int i = 0; i < row; i++) {
-                        text.set(i, "0" + text.get(i));
-                    }
-                }
-                if (text.get(row).charAt(text.get(row).length() - 1) == '1' &&
-                        text.get(row - 1).charAt(text.get(row).length() - 1) == '1'
-                        && text.get(row + 1).charAt(text.get(row).length() - 1) == '1') {
-                    text.set(row, text.get(row) + "1");
-                    for (int i = 0; i < row; i++) {
-                        text.set(i, text.get(i) + "0");
-                    }
-                }
-            }
 
-            //horizontal
-            text.add(0, "0");
-            text.add("0");
-            for (int col = 1; col < text.get(0).length() - 1; col++) {
-                if (text.get(0).charAt(col) == '1' && text.get(0).charAt(col - 1) == '1'
-                        && text.get(0).charAt(col + 1) == '1') {
-                    text.set(0, text.get(0) + "1");
-                } else {
-                    text.set(0, text.get(0) + "0");
+        //generation
+
+        //horizontal
+        for(int row = 0; row < text.size(); row += text.size() - 2){
+            for(int col = 1; col < text.get(row).length() - 1; col++){
+                if(text.get(row).charAt(col) == '1'
+                && text.get(row).charAt(col - 1) == '1'
+                && text.get(row).charAt(col + 1) == '1'){
+                    text.set(row + 1, text.get(row + 1).substring(0, col) + "1" + text.get(row + 1).substring(col + 1));
                 }
-                if (text.get(text.size() - 1).charAt(col) == '1'
-                        && text.get(text.size() - 1).charAt(col - 1) == '1'
-                        && text.get(text.size() - 1).charAt(col + 1) == '1') {
-                    text.set(text.size() - 1, text.get(text.size() - 1) + "1");
-                } else {
-                    text.set(text.size() - 1, text.get(text.size() - 1) + "0");
+
+                if(text.get(row + 1).charAt(col) == '1'
+                && text.get(row + 1).charAt(col - 1) == '1'
+                && text.get(row + 1).charAt(col + 1) == '1'){
+                    text.set(row, text.get(row).substring(0, col) + "1" + text.get(row).substring(col + 1));
                 }
             }
         }
-        catch (Exception e){
-            ;
+
+        //vertical
+        for(int row = 2; row < text.size() - 2; row++){
+            if(text.get(row).charAt(1) == '1'
+            && text.get(row - 1).charAt(1) == '1'
+            && text.get(row + 1).charAt(1) == '1'){
+                text.set(row, "1" + text.get(row).substring(1));
+            }
+        }
+        for(int row = 2; row < text.size() - 2; row++){
+            if(text.get(row).charAt(text.get(row).length() - 2) == '1'
+                    && text.get(row - 1).charAt(text.get(row).length() - 2) == '1'
+                    && text.get(row + 1).charAt(text.get(row).length() - 2) == '1'){
+                text.set(row, text.get(row).substring(0, text.get(row).length() - 1) + "1");
+            }
+        }
+        for(int i = 0; i < text.size(); i++){
+            for(int j = 0; j < text.get(i).length(); j++){
+                System.out.print(text.get(i).charAt(j) + " ");
+            }
+            System.out.println("");
         }
         return text;
     }
 
     private static ArrayList<String> clear(ArrayList<String> text){
-        try {
-            if (!text.get(0).contains("1")) {
-                text.remove(0);
+        //horizontal
+        ArrayList<String> buf = new ArrayList<String>();
+        int c = 0;
+        if(!text.get(0).contains("1")) {
+            c++;
+            text.remove(0);
+        }
+        if(!text.get(text.size() - 1).contains("1")){
+            c++;
+            text.remove(text.size() - 1);
+        }
+        if(!(c == 0)) {
+            for (int row = 0; row < text.size() - c; row++) {
+                buf.add(text.get(row));
             }
-            if (!text.get(text.size() - 1).contains("1")) {
-                text.remove(text.size() - 1);
-            }
-            boolean check1 = true;
-            boolean check2 = true;
-            for (int row = 0; row < text.size(); row++) {
-                if (text.get(row).charAt(0) == '1') {
-                    check1 = false;
-                    break;
-                }
-            }
-            for (int row = 0; row < text.size(); row++) {
-                if (text.get(row).charAt(text.get(row).length() - 1) == '1') {
-                    check2 = false;
-                    break;
-                }
-            }
-            if (check1) {
-                for (int row = 0; row < text.size(); row++) {
-                    text.set(row, text.get(row).substring(1));
-                }
-            }
-            if (check2) {
-                for (int row = 0; row < text.size(); row++) {
-                    text.set(row, text.get(row).substring(0, text.get(row).length() - 1));
-                }
+            text = buf;
+        }
+
+        //vertical
+        int c1 = 0;
+        int c2 = 0;
+        for(int row = 1; row < text.size() - 1; row++){
+            if(!(text.get(row).charAt(0) == '1')){
+                c1++;
+                System.out.println(c1);
             }
         }
-        catch (Exception e){
-            ;
+        for(int row = 1; row < text.size() - 1; row++){
+            if(!(text.get(row).charAt(text.get(row).length() - 1) == '1')){
+                c2++;
+            }
+        }
+        if(c1 == text.size() - 1){
+            for(int row = 1; row < text.size() - 2; row++){
+                text.set(row, text.get(row).substring(1));
+            }
+        }
+        if(c2 == text.size() - 1){
+            for(int row = 1; row < text.size() - 2; row++){
+                text.set(row, text.get(row).substring(0, text.get(row).length() - 1));
+            }
         }
         return text;
     }
