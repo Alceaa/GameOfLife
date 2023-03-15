627231ae153e3628db0d2d7e6fb1e22318a919b8
Alcea <maks.komendant@yandex.ru>
2023-03-15 13:44:39 +0300

    first successful

--- src/Logic.java
+++ src/Logic.java
@@ -2,10 +2,20 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static java.util.Arrays.deepHashCode;
+
 
 public class Logic {
-    private static List<Integer> states = Arrays.asList(0);
+    private static ArrayList<Integer> states = new ArrayList<Integer>();
     public static void cycle(char[][] cage) {
+        int state = deepHashCode(cage);
+        System.out.println(state);
+        char[][] cage_buf = new char[cage.length][cage[0].length];
+        for(int row = 0; row < cage.length; row++){
+            for(int col = 0; col < cage[row].length; col++){
+                cage_buf[row][col] = cage[row][col];
+            }
+        }
         for(int row = 0; row < cage.length; row++){
             for(int col = 0; col < cage[row].length; col++){
                 int neigh = 0;
@@ -66,17 +76,23 @@ public class Logic {
                 }
                 if(cage[row][col] == '0' || cage[row][col] == '\0'){
                     if(neigh == 3){
-                        System.out.println(1);
-                        cage[row][col] = '1';
+                        cage_buf[row][col] = '1';
                     }
                 }
                 else{
                     if(neigh < 2 || neigh > 3){
-                        cage[row][col] = '0';
+                        cage_buf[row][col] = '0';
                     }
                 }
             }
         }
+        if(states.contains(state)){
+            Main.setCondition(false);
+        }
+        else{
+            states.add(state);
+        }
+        cage = cage_buf;
         Main.setCage(cage);
         Main.start();
     }
